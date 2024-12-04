372909a9bdbec6e4e6c4aec504544628c5e084bc
Mohit <mohitshaw181104@gmail.com>
2024-12-04 17:15:15 +0530

    2

---
+++ functions.js
@@ -0,0 +1,6 @@
+function getValue(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 OK";
+}
---
+++ functions.ts
@@ -0,0 +1,6 @@
+function getValue(myVal: number): boolean{
+    if(myVal > 5){
+        return true
+    }
+    return "200 OK"
+}
