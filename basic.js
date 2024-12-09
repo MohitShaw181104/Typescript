226b194c329279ba92c70373eb7f7eabc10b503e
Mohit <mohitshaw181104@gmail.com>
2024-12-09 22:07:05 +0530

    7

--- basic.js
+++ basic.js
@@ -3,7 +3,7 @@ console.log("Hello Genius")
 var num1= 199;
 var num2= 799;
 
-var discount =((num2-num1) / num2*100);
+var discount =((num2-num5) / num2*100);
 console.log(discount);
 
 var answer= num1> num3;
